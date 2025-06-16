3ee5ebee627036420f453e48ec0e128f6f4e90ea
kazito <kauaalexsandro31@gmail.com>
2025-06-16 08:33:46 -0400

    implementa endpoint de login com autenticação de email e senha

--- src/main/java/io/github/kauaprojetos/controller/UserController.java
+++ src/main/java/io/github/kauaprojetos/controller/UserController.java
@@ -1,13 +1,16 @@
 package io.github.kauaprojetos.controller;
 
+import io.github.kauaprojetos.dto.LoginDTO;
 import io.github.kauaprojetos.dto.UserDTO;
 import io.github.kauaprojetos.model.UserModel;
 import io.github.kauaprojetos.repository.UserRepository;
 import io.github.kauaprojetos.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -27,14 +30,27 @@ public class UserController {
         return userService.listUser();
     }
 
-    @PostMapping("save")
-    public void saveUser(@RequestBody UserDTO dto){
+    @PostMapping("/save")
+    public ResponseEntity<?> salvar(@RequestBody UserDTO dto) {
         userService.saveUser(dto);
+        return ResponseEntity.ok(Map.of("message", "Usuário cadastrado com sucesso!"));
     }
+
 
     @DeleteMapping("/delete/{id}")
     public void deleteUser(@PathVariable UUID id){
         userService.deleteUser(id);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginDTO dto) {
+        UserModel user = userService.autenticar(dto.email(), dto.senha());
+        return ResponseEntity.ok(Map.of(
+                "message", "Login realizado com sucesso!",
+                "nome", user.getName(),
+                "email", user.getEmail(),
+                "cpf", user.getCpf()
+        ));
     }
 
 }
